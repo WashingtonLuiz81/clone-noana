8c15b537f013898771853f51e8e51e226cd2d3a6
wlsdesign <wlsdesign81@gmail.com>
2024-08-06 16:59:32 -0300

    fix: resolvido cor do bg do icone do componente ResolveAlert

--- src/components/resolveAlert.tsx
+++ src/components/resolveAlert.tsx
@@ -11,7 +11,7 @@ export default function ResolveAlert({ status }: ResolveAlertProps) {
       className={`flex items-center gap-4 p-4 rounded-xl border ${status ? 'border-green-600 bg-green-50' : 'border-status-error bg-red-50'}`}
     >
       <span
-        className={`w-10 h-10 flex items-center justify-center rounded-xl ${status ? 'text-green-700 bg-gray-200' : 'text-red-600 bg-red-100'}`}
+        className={`w-10 h-10 flex items-center justify-center rounded-xl ${status ? 'text-green-700 bg-green-200' : 'text-red-600 bg-red-100'}`}
       >
         {status ? <CheckIcon /> : <AlertTriangleIcon />}
       </span>
